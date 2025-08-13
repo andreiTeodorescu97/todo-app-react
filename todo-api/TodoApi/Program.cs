a592bd2d9a326a20d1060c68ccaafeb29145400d
andreiTeodorescu97 <andreiTeodorescu97>
2025-08-13 19:13:23 +0300

    small changes

--- todo-api/TodoApi/Program.cs
+++ todo-api/TodoApi/Program.cs
@@ -5,25 +5,15 @@ using TodoApi.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddFastEndpoints();
-
-// Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Add Entity Framework Core
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<TodoDbContext>(options =>
     options.UseSqlServer(connectionString));
-
-// Register application services
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<TodoService>();
 builder.Services.AddScoped<CategoryService>();
-
-// Add CORS
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -35,16 +25,12 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseCors();
 app.UseMiddleware<AuthMiddleware>();
 app.UseFastEndpoints();
-
 app.Run();
